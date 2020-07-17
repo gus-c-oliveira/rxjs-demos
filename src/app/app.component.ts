a3606fc9457c39ae869601d2227fd9e76e1f306b
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-07-17 08:39:51 -0300

    App: add Header component to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AppConstants } from './app.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public readonly appTitle = AppConstants.AppTitle;
+}
