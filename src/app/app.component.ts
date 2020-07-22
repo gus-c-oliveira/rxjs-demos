51a87cfa87ef30c0359edb25d930645fb1df0bf2
Gustavo C. Oliveira <gus.c.oliveira@outlook.com>
2020-07-22 20:25:36 -0300

    Demos: demo for manually created Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { AppConstants } from './app.constants';
 
 @Component({
@@ -8,14 +10,29 @@ import { AppConstants } from './app.constants';
 })
 export class AppComponent {
   public readonly appTitle = AppConstants.AppTitle;
-  public demos = [
-    { id: 0, title: 'Demo 1' },
-    { id: 1, title: 'Demo 2' },
-  ];
-  public code = Array(25).fill('code');
-  private selectedDemo = null;
+  public listOfDemos: { id: number; title: string }[] = [];
+  public description = '';
+  public code: string[] = [];
+  public demoOutput: any[] = [];
+  public selectedDemo: number = null;
+  private subscription: Subscription = null;
+
+  public constructor() {
+    AppConstants.Demos.forEach((demo, index) => {
+      this.listOfDemos.push({ id: index, title: demo.title });
+    });
+  }
 
   public updateSelection(selected) {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.subscription = null;
+    }
     this.selectedDemo = selected;
+    this.description = AppConstants.Demos[this.selectedDemo].description;
+    this.code = AppConstants.Demos[this.selectedDemo].code;
+    const runArtifacts = AppConstants.Demos[this.selectedDemo].run();
+    this.subscription = runArtifacts.subscription;
+    this.demoOutput = runArtifacts.results;
   }
 }
--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,28 @@
+import { Observable } from 'rxjs';
+
 export const AppConstants = {
   AppTitle: 'RxJS Demos',
+  Demos: [
+    {
+      title: 'Create Manually',
+      description:
+        'An Observable represents a source of data that emits values over time. When creating one manually, you can subscribe to it and use the .next() function to emit values.',
+      code: [
+        'const observable = new Observable(observer => {',
+        "\xa0\xa0\xa0\xa0observer.next('Hello Observable!');",
+        '});',
+        'observable.subscribe(value => log(value));',
+      ],
+      run: () => {
+        const observable = new Observable((observer) => {
+          observer.next('Hello Observable!');
+        });
+        const results = [];
+        const subscription = observable.subscribe((value) =>
+          results.push(value)
+        );
+        return { results, subscription };
+      },
+    },
+  ],
 };
